967d9fa60df7763c155cf7e12827b0384f6ab73f
dickanirwansyah <dickanirwansyah@gmail.com>
2024-07-08 20:58:55 +0700

    initialize project

---
+++ src/test/java/com/app/spring_boot_jenkins/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.spring_boot_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
